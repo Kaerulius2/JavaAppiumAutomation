39a0bed83014eea65027dc86e22ecfc91cfa9f96
kaerulius2@yandex.ru <kaerulius2@yandex.ru>
2020-12-16 22:59:26 +0300

    swipeUptoFindEl

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,16 +1,21 @@
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
+import java.time.Duration;
 import java.util.List;
 
 public class FirstTest {
@@ -61,6 +66,23 @@ public class FirstTest {
         String article_title = title_element.getAttribute("text");
 
         Assert.assertEquals("We see unexpected Article title","Java (programming language)", article_title);
+    }
+
+    @Test
+    public void testSwipeArticle() throws InterruptedException {
+
+        waitForElementAndClick(By.id("org.wikipedia:id/fragment_onboarding_skip_button"),"Cannot find SKIP",5);
+        waitForElementAndClick(By.xpath("//*[contains(@text, 'Search Wikipedia')]"), "Cannot find search element", 3);
+
+        waitForElementAndSendKeys(By.xpath("//*[contains(@text, 'Search Wikipedia')]"), "Appium", "Cannot find search input", 3);
+
+        waitForElementAndClick(By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='Appium']"), "Error finding Appium in search", 3);
+        //android.view.View
+        waitForElementPresent(By.xpath("//*[@class='android.view.View'][@text='Appium']"),"Cannot find article title",10);
+
+
+        swipeUpToFindElement(By.xpath("//*[@class='android.widget.TextView'][@text='View article in browser!!!']"),"Cannot find the end of article",20);
+
     }
     @Test
     public void testCancelSearch()
@@ -127,6 +149,35 @@ public class FirstTest {
         return count;
     }
 
+    protected void swipeUp(int timeOfSwipe)
+    {
+        TouchAction action = new TouchAction(driver);
+        Dimension size = driver.manage().window().getSize();
+        int x = size.width/2;
+        int start_y = (int)(size.height*0.8);
+        int end_y = (int)(size.height*0.2);
+
+        action.press(PointOption.point(x,start_y)).waitAction(WaitOptions.waitOptions(Duration.ofMillis(timeOfSwipe))).moveTo(PointOption.point(x,end_y)).release().perform();
+    }
+
+    protected void swipeUpQuick()
+    {
+        swipeUp(200);
+    }
+
+    protected void swipeUpToFindElement(By by, String error_message, int max_swipes)
+    {
+        int already_swiped = 0;
+        while(driver.findElements(by).size()==0)
+        {
+            if(already_swiped>max_swipes){
+                waitForElementPresent(by,"Cannot find element by swiping up. \n" + error_message,0);
+                return;
+            }
+            swipeUpQuick();
+            ++already_swiped;
+        }
+    }
 
     private void assertElementHasText(By by, String text, String error_message)
     {
